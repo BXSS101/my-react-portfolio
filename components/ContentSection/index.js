cbb4cc1f0a82f4779a87417135aa544749e01405
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2023-06-26 09:57:09 +0700

    Added 2 more projects

--- components/ContentSection/index.js
+++ components/ContentSection/index.js
@@ -25,7 +25,7 @@ const CodeBlock = {
 
 const ContentSection = ({ content }) => {
   return (
-    <ReactMarkdown components={CodeBlock} escapeHtml={false} className="markdown-class">
+    <ReactMarkdown components={CodeBlock} className="markdown-class">
       {content}
     </ReactMarkdown>
   );
